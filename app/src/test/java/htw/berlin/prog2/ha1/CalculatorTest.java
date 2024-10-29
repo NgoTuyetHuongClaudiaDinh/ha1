a70ee3b8532a860ea67422771ca2ef08ddc1a662
NgoTuyetHuongClaudiaDinh <claudiatuyethuong01@yahoo.de>
2024-10-29 11:47:16 +0100

    Teilaufgabe2

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -91,18 +91,17 @@ class CalculatorTest {
 
     //TODO hier weitere Tests erstellen
 
-/**
+    /**
     * Teilaufgabe 1: Schreiben Sie einen neuen zusätzlichen Test,
     * der eine bisher nicht getestete Funktionalität abdeckt, die
     * bereits funktioniert und der daher direkt grün wird.
-    * Subtaktion
+    * Testfall: Subtaktion von positiven Zahlen
     */
 
     @Test
     @DisplayName("should display result after subtracting two positive multi-digit numbers")
       void testPositiveSubtraction() {
           Calculator calc = new Calculator();
-   
    
           calc.pressDigitKey(2);
           calc.pressDigitKey(0);
@@ -111,13 +110,57 @@ class CalculatorTest {
           calc.pressDigitKey(0);
           calc.pressEqualsKey();
    
-   
           String expected = "10";
           String actual = calc.readScreen();
    
-   
           assertEquals(expected, actual);
-      }
-   }
-   
+    }
+
+    /**
+    * Teilaufgabe 2: Schreiben Sie zwei weitere zusätzliche Tests, 
+    * die zwei unterschiedliche Fehlerkategorien aufdecken (d.h. deren 
+    * Fehlerursachen in unterschiedlichen Methoden liegen) und somit fehlschlagen.
+    */
+
+    //Testfall: Nur die letzte Eingabe soll gelöscht werden 
+    //prüft die pressClearKey-Methode, um sicherzustellen, dass nur die letzte 
+    //und nicht die gänzliche Eingabe gelöscht wird
+
+    @Test
+    @DisplayName("should only delete last number entered when pressed once")
+    void testClearKey() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(5);
+        calc.pressClearKey();
+        calc.pressDigitKey(7);
+        calc.pressEqualsKey();
+
+        String expected = "14";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    //Testfall: Eingabe von einzelner Zahl, soll auch einzelne Zahl im Ergebnis anzeigen 
+    //und nicht als Error angezeigt werden 
+    //prüft die pressEqualsrKey-Methode, um sicherzustellen, dass bei Eingabe einer Zahl ohne 
+    //Operation auch die eingegebene Zahl, unverändert im Ergebnis anzeigt wird
+
+    @Test
+    @DisplayName("should display the same number when equals is pressed without an operation")
+     void testEqualsWithoutOperation() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "3"; 
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+}
    
